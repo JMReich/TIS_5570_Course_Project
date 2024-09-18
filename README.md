3af29dcf2d06e6b439a0af338a1cf15f35687a84
BiggerOwO <149850274+JMReich@users.noreply.github.com>
2024-09-18 19:42:04 -0400

    Initial Commit
    This is the initial commit for the project just to outline our work.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TIS_5570_Course_Project
 This repository is for TIS-5570 Group 3, course project. 
+
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+![alt text](image-2.png)
+
+
+
+
+
+
+
+
+
---
+++ python/main.py
@@ -0,0 +1,68 @@
+# This is how you add a comment in Python
+
+'''
+This is also a comment in Python, but in this case it is a multi-line comment
+
+Just don't forget to close it with three single quotes.
+
+'''
+
+
+
+
+'''
+General Information:
+
+
+At a minimum, you will need to answer three of the following business questions:
+1. What is the expected selling price of my home?
+2. What factors influence the price of my home?
+3. Which factors are more important than others?
+4. How much should I invest in improving the condition of my home in order to
+increase the expected price by more than the cost of improvements?
+5. Which homes should I compare my house to?
+6. When is the best time of the year to sell my home?
+
+You are encouraged to create additional questions and answer them. Your goal is to
+bring insights and questions that the AREA team bas not thought about.
+
+
+
+Dataset and Supporting Files
+The Ames dataset that you must use in this project is in a Canvas module called
+"Course Project Files". In this module, you will find the following files:
+Ames.xlsx: This is the ames data set
+- Ames Data Dictionary-final.pdf: This is a file containing a description of the
+data elements/columns contained in the ames.xlsx file
+- Modeling Home Prices Using Realtor Data.pdf: This article provides insights
+into how to model home prices.
+- Modeling references.pdf: This file contains links to references about how to
+analyze the ames data.
+- You can also do your research and find other material related to the ames
+dataset. Please reference material that is not your work and include a
+citation in your report.
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
